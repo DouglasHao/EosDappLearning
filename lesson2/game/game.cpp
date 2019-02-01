e7188a9a10a851332b72ad407c6af1efd3d6322f
haibin <1966235895@qq.com>
2019-02-01 17:11:40 +0800

    add game

---
+++ lesson2/game/game.cpp
@@ -0,0 +1,71 @@
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/print.hpp>
+#include <eosiolib/asset.hpp>
+#include <eosiolib/action.hpp>
+#include <eosiolib/symbol.hpp>
+#include <eosiolib/singleton.hpp>
+#include <eosiolib/print.hpp>
+#include <eosiolib/transaction.hpp>
+#include <eosiolib/crypto.h> 
+#include <eosiolib/dispatcher.hpp>
+
+using namespace eosio;
+
+class playgame : public contract {
+public:
+	using contract::contract;
+	using namespace std;
+
+	[[eosio::action]]
+	void game_transfer( name user ) {
+		require_auth(_self);
+		auto values = user.value;
+		print("game user value :", values);
+		print(", gameuser: ", user);
+
+		//内联调用 语法说明
+		//action(
+		//permission_level, //权限
+		//code, 合约
+		//action, 合约方法
+		//data); 调用数据
+
+		action(permission_level{_self, name("active")},
+              	name("eosio.token"), name("transfer"),
+                std::make_tuple(_self, user, asset(1, symbol("HELLO", 2)),
+                                std::string("game send  hello")) ).send();
+	}
+
+	//延时调用
+	[[eosio::action]]
+	void delay_game(string memo) {
+		eosio::transaction t{};
+		t.actions.emplace_back(eosio::permission_level(_self,name("active")),
+								name("playgame"),name("game_transfer"),
+								std::make_tuple(name("game")));
+		t.delay_sec = 1;
+		t.send(1, _self, false);
+		print("delay end");
+	}
+
+	[[eosio::action]]
+	void transfer(name from, name to, asset quantity, string memo) {
+		print("transfer from", from);
+		eosio_assert(0," transfer error");
+	}		
+};
+
+#define EOSIO_DISPATCH_CUSTOM(TYPE, MEMBERS) \
+extern "C" { \
+   void apply( uint64_t receiver, uint64_t code, uint64_t action ) { \
+   auto self = receiver; \
+      if(( code == self&&action != name("transfer").value) ) { \
+        switch( action ) { \
+            EOSIO_DISPATCH_HELPER( TYPE, MEMBERS ) \
+         } \
+         /* does not allow destructor of this contract to run: eosio_exit(0); */ \
+      } \
+   } \
+} \
+
+EOSIO_DISPATCH( playgame, (game_transfer)(delay_game)(transfer))
